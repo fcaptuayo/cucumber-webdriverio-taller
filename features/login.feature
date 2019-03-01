49e2ea6d7d911c1bfdf4f6fc3976ecd78164ab68
fcaptuayo <captuayonovaofredy@gmail.com>
2019-02-28 20:58:56 -0500

    first commit

--- features/login.feature
+++ features/login.feature
@@ -1,1 +1,41 @@
-#Complete siguiendo las instrucciones del taller.
+Feature: Login into losestudiantes
+    As an user I want to authenticate myself within losestudiantes website in order to rate teachers
+
+Scenario Outline: Login failed with wrong inputs
+
+  Given I go to losestudiantes home screen
+    When I open the login screen
+    And I fill with <email> and <password>
+    And I try to login
+    Then I expect to see <error>
+
+    Examples:
+      | email            | password | error                    |
+      |                  |          | Ingresa una contraseña   |
+      | miso@gmail.com   |    1234  | Upss! El correo y        |
+
+
+Scenario Outline: Login sucess with corect inputs
+
+  Given I go to losestudiantes home screen
+    When I open the login screen
+    And I fill sucess with <email> and <password>
+    And I try to login
+    Then I expect to be able to login sucess
+
+    Examples:
+      | email                          | password     |
+      | captuayonovoafredy@gmail.com   |    1234567890|
+
+
+Scenario Outline: Register sucess with corect inputs
+
+  Given I go to losestudiantes home screen
+    When I open the login screen
+    And I fill register with <name> and <lastname>
+    And I try to register
+    Then I expect to be able to register sucess
+
+    Examples:
+      | name                          | lastname     |
+      | fredy   					  |    1234567890|
--- features/step-definitions/index.js
+++ features/step-definitions/index.js
@@ -1,1 +1,94 @@
-//Complete siguiendo las instrucciones del taller
+var {defineSupportCode} = require('cucumber');
+var {expect} = require('chai');
+
+defineSupportCode(({Given, When, Then}) => {
+  Given('I go to losestudiantes home screen', () => {
+    browser.url('/');
+    //if(browser.isVisible('button=Cerrar')) {
+    //  browser.click('button=Cerrar');
+    //}
+  });
+
+  When('I open the login screen', () => {
+    browser.waitForVisible('button=Ingresar', 5000);
+    browser.click('button=Ingresar');
+  });
+
+  When(/^I fill with (.*) and (.*)$/ , (email, password) => {
+     var cajaLogIn = browser.element('.cajaLogIn');
+
+    var mailInput = cajaLogIn.element('input[name="correo"]');
+    mailInput.click();
+    mailInput.keys(email);
+
+    var passwordInput = cajaLogIn.element('input[name="password"]');
+    passwordInput.click();
+    passwordInput.keys(password)
+  });
+
+Then('I expect to see {string}', error => {
+    browser.waitForVisible('.aviso.alert.alert-danger', 10000);
+    var alertText = browser.element('.aviso.alert.alert-danger').getText();
+    expect(alertText).to.include(error);
+  });
+
+  When('I fill a wrong email and password', () => {
+    var cajaLogIn = browser.element('.cajaLogIn');
+
+    var mailInput = cajaLogIn.element('input[name="correo"]');
+    mailInput.click();
+    mailInput.keys('wrongemail@example.com');
+
+    var passwordInput = cajaLogIn.element('input[name="password"]');
+    passwordInput.click();
+    passwordInput.keys('123467891')
+  });
+
+  When('I try to login', () => {
+    var cajaLogIn = browser.element('.cajaLogIn');
+    cajaLogIn.element('button=Ingresar').click()
+  });
+
+  Then('I expect to not be able to login', () => {
+    browser.waitForVisible('.aviso.alert.alert-danger', 10000);
+  });
+
+  When(/^I fill sucess with (.*) and (.*)$/ , (email, password) => {
+     var cajaLogIn = browser.element('.cajaLogIn');
+
+    var mailInput = cajaLogIn.element('input[name="correo"]');
+    mailInput.click();
+    mailInput.keys(email);
+
+    var passwordInput = cajaLogIn.element('input[name="password"]');
+    passwordInput.click();
+    passwordInput.keys(password)
+  });
+
+  Then('I expect to be able to login sucess', () => {
+    var cajaLogIn = browser.element('.cuenta');
+  });
+
+  When('I try to register', () => {
+    var cajaLogIn = browser.element('.cajaSignUp');
+    cajaLogIn.element('button=Registrarse').click()
+  });
+
+  When(/^I fill register with (.*) and (.*)$/ , (name, lastname) => {
+     var cajaLogIn = browser.element('.cajaSignUp');
+
+    var mailInput = cajaLogIn.element('input[name="nombre"]');
+    mailInput.click();
+    mailInput.keys(name);
+
+    var passwordInput = cajaLogIn.element('input[name="apellido"]');
+    passwordInput.click();
+    passwordInput.keys(lastname)
+  });
+
+  Then('I expect to be able to register sucess', () => {
+    var cajaLogIn = browser.element('.cuenta');
+  });
+
+
+});
